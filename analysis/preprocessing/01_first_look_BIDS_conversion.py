5456baab4d38e0d435b735926fb31e9c3c64ea14
tghafari <tara.ghafari@gmail.com>
2024-03-08 17:48:57 +0000

    attempt to artifact annotation

--- analysis/preprocessing/01_first_look_BIDS_conversion.py
+++ analysis/preprocessing/01_first_look_BIDS_conversion.py
@@ -14,6 +14,7 @@ t.ghafari@bham.ac.uk
 # Import relevant Python modules
 import os.path as op
 import os
+import pandas as pd
 
 import mne
 from mne_bids import (BIDSPath, write_raw_bids, read_raw_bids)
@@ -24,6 +25,7 @@ subj_code = '01_ly'  # subject code assigned to by Benchi's group
 subj_name = 'Liuyu'  # name on eeg file
 platform = 'mac'  # are you using 'bluebear', 'mac', or 'windows'?
 pilot = True  # is it pilot data or real data?
+rprt = True
 
 if platform == 'bluebear':
     rds_dir = '/rds/projects/j/jenseno-avtemporal-attention'
@@ -48,8 +50,12 @@ session = '01'
 task = 'SpAtt'
 run = '01'
 
+# BIDS events
+events_suffix = 'events'  
+events_extension = '.tsv'
+
 # Read raw file in BrainVision (.vhdr, .vmrk, .eeg) format
-raw = mne.io.read_raw_brainvision(vhdr_fname, preload=False)
+raw = mne.io.read_raw_brainvision(vhdr_fname, eog=('HEOGL', 'HEOGR', 'VEOGb'), preload=False)
 
 # Plot raw to take a look
 raw.plot(title="raw") 
@@ -59,7 +65,82 @@ events, events_id = mne.events_from_annotations(raw, event_id='auto')
 mne.write_events(events_fname, events, overwrite=True)
 
 # Convert to BIDS
-bids_path = BIDSPath(subject=subject, session=session,
+bids_path = BIDSPath(subject=subject, session=session, datatype ='eeg',
                      task=task, run=run, root=bids_root)
 write_raw_bids(raw, bids_path, events_data=events_fname, 
                event_id=events_id, overwrite=True)
+
+# Plot triggers from bids .tsv file
+events_bids_path = bids_path.copy().update(suffix=events_suffix,
+                                            extension=events_extension)
+events_file = pd.read_csv(events_bids_path, sep='\t')
+event_onsets = events_file[['onset', 'value', 'trial_type']]
+
+# Plot all events
+event_onsets.plot(kind='scatter', x='onset', y='trial_type')
+plt.xlabel('onset(sec)')
+plt.ylabel('event type')
+plt.show()
+
+if rprt:
+    report_root = op.join(project_root, r'results/reports')  # RDS folder for reports
+    if not op.exists(op.join(report_root , 'sub-' + subject)):
+        os.makedirs(op.join(report_root , 'sub-' + subject))
+    report_folder = op.join(report_root , 'sub-' + subject)
+    report_fname = op.join(report_folder, f'sub-{subject}_events.hdf5')    # it is in .hdf5 for later adding images
+    html_report_fname = op.join(report_folder, f'sub_{subject}_events.html')
+
+    report = mne.Report(title=f'sub-{subject}')
+    report.add_events(events=events, title='events from "events"', sfreq=raw.info['sfreq'])
+    report.save(report_fname, overwrite=True)
+    report.save(html_report_fname, overwrite=True, open_browser=True)  # to check how the report looks
+        
+
+""" this section needs to be edited with new stim names from Benchi
+# Check durations using triggers
+durations_onset = ['cue', 'catch', 'stim', 'dot', 'response_press']
+durations_offset = ['cue'] #, 'stim', 'dot']  # stim and dot are removed in actual data collection
+direction_onset = ['cue_onset', 'dot_onset']
+events_dict = {}
+
+for dur in durations_onset:    
+    events_dict[dur + "_onset"] = event_onsets.loc[event_onsets['trial_type'].str.contains(f'{dur}_onset'),
+                                                'onset'].to_numpy()
+for dur in durations_offset:   
+    events_dict[dur + "_offset"] = event_onsets.loc[event_onsets['trial_type'].str.contains(f'{dur}_offset'),
+                                                'onset'].to_numpy()
+for dirs in direction_onset:
+    events_dict[dirs + "_right"] = event_onsets.loc[event_onsets['trial_type'].str.contains(f'{dirs}_right'),
+                                                'onset'].to_numpy()
+    events_dict[dirs + "_left"] = event_onsets.loc[event_onsets['trial_type'].str.contains(f'{dirs}_left'),
+                                                'onset'].to_numpy()
+
+# compare number of trials with stimuli and responses
+numbers_dict = {}
+for numbers in  ['cue_onset_right', 'cue_onset_left', 'dot_onset_right', 'dot_onset_left', 
+                        'response_press_onset']:
+    numbers_dict[numbers] = events_dict[numbers].size
+    
+fig, ax = plt.subplots()
+bars = ax.bar(range(len(numbers_dict)), list(numbers_dict.values()))
+plt.xticks(range(len(numbers_dict)), list(numbers_dict.keys()), rotation=45)
+ax.bar_label(bars)
+plt.show()
+
+# Check duration of cue presentation  
+events_dict['cue_duration'] = events_dict['cue_offset'] - events_dict['cue_onset']
+events_dict['stim_to_dot_duration'] = events_dict['dot_onset'] - events_dict['stim_onset']
+events_dict['RT'] = events_dict['response_press_onset'] - events_dict['dot_onset'] 
+# cheat line to add extra elements for easier calculation of RTs:
+    #events_dict['dot_onset'] = np.insert(events_dict['dot_onset'], index_onset_should_be_added_before, onset)
+
+# Plot all durations
+for dur in ['cue_duration', 'stim_to_dot_duration', 'RT']:
+    fig, ax = plt.subplots()
+    plt.hist(events_dict[dur])
+    plt.title(dur)
+    plt.xlabel('time in sec')
+    plt.ylabel('number of events')
+    plt.show()
+"""
+
---
+++ analysis/preprocessing/06_artifact_annotation.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================
+06. Annotation of artifacts
+
+This code will identify artifacts and then annotate
+them for later use (eg., to reject).
+
+written by Tara Ghafari
+adapted from flux pipeline
+==============================================
+"""
+
+import os.path as op
+import os
+import numpy as np
+
+import mne
+from mne.preprocessing import annotate_muscle_zscore
+from mne_bids import BIDSPath, read_raw_bids
+
+# BIDS settings: fill these out 
+subject = '01'
+session = '01'
+task = 'SpAtt'
+run = '01'
+eeg_suffix = 'eeg'
+eeg_extension = '.vhdr'
+deriv_suffix = 'ann'
+
+platform = 'mac'  # are you using 'bluebear', 'mac', or 'windows'?
+pilot = True  # is it pilot data or real data?
+rprt = True
+
+if platform == 'bluebear':
+    rds_dir = '/rds/projects/j/jenseno-avtemporal-attention'
+elif platform == 'mac':
+    rds_dir = '/Volumes/jenseno-avtemporal-attention'
+
+project_root = op.join(rds_dir, 'Projects/subcortical-structures/STN-in-PD')
+if pilot:
+    data_root = op.join(project_root, 'Data/pilot-data/AO')
+else:
+    data_root = op.join(project_root, 'Data/real-data')
+
+# Specify specific file names
+bids_root = op.join(project_root, 'Data', 'BIDS')
+bids_path = BIDSPath(subject=subject, session=session,
+                     task=task, run=run, root=bids_root, 
+                     datatype ='eeg', suffix=eeg_suffix,)
+deriv_folder = op.join(bids_root, 'derivatives', 'sub-' + subject)  # RDS folder for results
+if not op.exists(deriv_folder):
+    os.makedirs(deriv_folder)
+
+deriv_fname = bids_path.basename + '_' + deriv_suffix
+
+# Read raw data 
+raw = read_raw_bids(bids_path=bids_path, verbose=False, 
+                     extra_params={'preload':True})
+
+"""
+# Identifying and annotating eye blinks using vEOG (EOG001)
+raw.copy().pick_channels(ch_names=['EOG001','EOG002'   # vEOG, hEOG, EKG
+                                       ,'ECG003']).plot()  # 'plot to make sure channel' 
+                                                           # 'names are correct, rename otherwise'
+eog_events = mne.preprocessing.find_eog_events(raw_sss, ch_name='EOG001')
+onset = eog_events[:,0] / raw_sss.info['sfreq'] -.25 #'from flux pipline, but why?'
+                                                     # 'blink onsets in seconds'
+onset -= raw_sss.first_time  # first_time is apparently the time start time of the raw data
+n_blinks = len(eog_events)  # length of the event file is the number of blinks in total
+duration = np.repeat(.5, n_blinks)  # duration of each blink is assumed to be 500ms
+description = ['blink'] * n_blinks
+annotation_blink = mne.Annotations(onset, duration, description)
+"""
+
+# Identifying and annotating muscle artifacts
+""" 
+muscle artifacts are identified from the magnetometer data filtered and 
+z-scored in filter_freq range
+"""
+threshold_muscle = 10
+min_length_good = .2
+filter_freq = [110,140]
+annotation_muscle, scores_muscle = annotate_muscle_zscore(raw, 
+                                                          ch_type=None, 
+                                                          threshold=threshold_muscle, 
+                                                          min_length_good=min_length_good, 
+                                                          filter_freq=filter_freq)
+annotation_muscle.onset -= raw.first_time  # align the artifact onsets to data onset
+annotation_muscle._orig_time = None  # remove date and time from the annotation variable
+
+# Include annotations in dataset and inspect
+raw_sss.set_annotations(annotation_blink + annotation_muscle)
+raw_sss.set_channel_types({'EOG001':'eog', 'EOG002':'eog', 'ECG003':'ecg'})  # set both vEOG and hEOG as EOG channels
+eog_picks = mne.pick_types(raw.info, meg=False, eog=True)
+scale = dict(eog=500e-6)
+raw_sss.plot(order=eog_picks, scalings=scale, start=50)
+
+# Save the artifact annotated file
+raw_sss.save(deriv_fname, overwrite=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
