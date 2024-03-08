4feb362d965cdc5109aa5185f6889defe0a73e79
tghafari <tara.ghafari@gmail.com>
2024-03-08 15:37:08 +0000

    added analysis folder

---
+++ analysis/preprocessing/00_first_look.py
@@ -0,0 +1,12 @@
+"""
+00_first_look
+in this code we try to open .eeg file
+and take a looka at raw eeg data.
+then we test trigger timings and identity.
+
+written by Tara Ghafari
+t.ghafari@bham.ac.uk
+"""
+
+
+import mne 
