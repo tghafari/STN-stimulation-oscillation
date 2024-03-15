f7db018d06fb3302cd1705291b424da04558440a
tghafari <tara.ghafari@gmail.com>
2024-03-15 18:46:00 +0000

    renaming triggers

--- analysis/preprocessing/01_first_look_BIDS_conversion.py
+++ analysis/preprocessing/01_first_look_BIDS_conversion.py
@@ -59,16 +59,63 @@ raw = mne.io.read_raw_brainvision(vhdr_fname, eog=('HEOGL', 'HEOGR', 'VEOGb'), p
 
 # Plot raw to take a look
 raw.plot(title="raw") 
+n_fft = int(raw.info['sfreq']*2)  # to ensure window size = 2
+raw.compute_psd(n_fft=n_fft, n_overlap=int(n_fft/2)).plot()  # default method is welch here (multitaper for epoch)
 
 # Read events from raw object
-events, events_id = mne.events_from_annotations(raw, event_id='auto')
+events, _ = mne.events_from_annotations(raw, event_id='auto')
+# Create Annotation object with correct labels
+"""list of triggers https://github.com/tghafari/STN-stimulation-oscillation/blob/main/Instructions/triggers.md"""
+mapping = {1:'cue_onset_right',
+           2:'cue_onset_left',
+           3:'trial_onset',
+           4:'stim_onset',
+           5:'catch_onset',
+           6:'dot_onset_right',
+           7:'dot_onset_left',
+           8:'response_press_onset',
+           20:'block_onset',
+           21:'block_end',
+           30:'experiment_end',
+           99999:'new_stim_segment',
+        }
+annotations_from_events = mne.annotations_from_events(events=events,
+                                                    event_desc=mapping,
+                                                    sfreq=raw.info["sfreq"],
+                                                    orig_time=raw.info["meas_date"],
+                                                    )
+raw.set_annotations(annotations_from_events)
+
+# Have to explicitly assign values to events 
+event_dict = {'cue_onset_right':1,
+           'cue_onset_left':2,
+           'trial_onset':3,
+           'stim_onset':4,
+           'catch_onset':5,
+           'dot_onset_right':6,
+           'dot_onset_left':7,
+           'response_press_onset':8,
+           'block_onset':20,
+           'block_end':21,
+           'experiment_end':30,
+           'new_stim_segment':99999,
+        }
+_, events_id = mne.events_from_annotations(raw, event_id=event_dict)
+
+# Plot raw to make sure the events are correct
+raw.plot(title="raw") 
 mne.write_events(events_fname, events, overwrite=True)
 
 # Convert to BIDS
 bids_path = BIDSPath(subject=subject, session=session, datatype ='eeg',
                      task=task, run=run, root=bids_root)
+
+# Write to BIDS format
 write_raw_bids(raw, bids_path, events_data=events_fname, 
                event_id=events_id, overwrite=True)
+
+# Plot all events
+fig = mne.viz.plot_events(events, sfreq=raw.info["sfreq"], first_samp=raw.first_samp, event_id=events_id)
 
 # Plot triggers from bids .tsv file
 events_bids_path = bids_path.copy().update(suffix=events_suffix,
@@ -76,11 +123,6 @@ events_bids_path = bids_path.copy().update(suffix=events_suffix,
 events_file = pd.read_csv(events_bids_path, sep='\t')
 event_onsets = events_file[['onset', 'value', 'trial_type']]
 
-# Plot all events
-event_onsets.plot(kind='scatter', x='onset', y='trial_type')
-plt.xlabel('onset(sec)')
-plt.ylabel('event type')
-plt.show()
 
 if rprt:
     report_root = op.join(project_root, r'results/reports')  # RDS folder for reports
@@ -91,15 +133,19 @@ if rprt:
     html_report_fname = op.join(report_folder, f'sub_{subject}_events.html')
 
     report = mne.Report(title=f'sub-{subject}')
-    report.add_events(events=events, title='events from "events"', sfreq=raw.info['sfreq'])
+    report.add_events(events=events, 
+                      event_id=events_id, 
+                      tags=('eve'),
+                      title='events from "events"', 
+                      sfreq=raw.info['sfreq'])
     report.save(report_fname, overwrite=True)
     report.save(html_report_fname, overwrite=True, open_browser=True)  # to check how the report looks
         
 
-""" this section needs to be edited with new stim names from Benchi
-# Check durations using triggers
+# this section needs to be edited with new stim names from Benchi
+# Check durations using triggers !!!!! number of events from tsv fo not match events_from_annotation from above. why? maybe run everything from beginnig to test.
 durations_onset = ['cue', 'catch', 'stim', 'dot', 'response_press']
-durations_offset = ['cue'] #, 'stim', 'dot']  # stim and dot are removed in actual data collection
+durations_offset = ['cue'] 
 direction_onset = ['cue_onset', 'dot_onset']
 events_dict = {}
 
@@ -128,7 +174,7 @@ ax.bar_label(bars)
 plt.show()
 
 # Check duration of cue presentation  
-events_dict['cue_duration'] = events_dict['cue_offset'] - events_dict['cue_onset']
+events_dict['dot_onset'] = events_dict['dot_onset_right','dot_onset_left']
 events_dict['stim_to_dot_duration'] = events_dict['dot_onset'] - events_dict['stim_onset']
 events_dict['RT'] = events_dict['response_press_onset'] - events_dict['dot_onset'] 
 # cheat line to add extra elements for easier calculation of RTs:
@@ -142,5 +188,5 @@ for dur in ['cue_duration', 'stim_to_dot_duration', 'RT']:
     plt.xlabel('time in sec')
     plt.ylabel('number of events')
     plt.show()
-"""
+
 
