ea5de7f94b1f97301f393d2db3148573d99baeda
tghafari <tara.ghafari@gmail.com>
2024-07-24 20:03:40 +0100

    lfp to bids done

--- analysis/LFP/01_first_look.py
+++ analysis/LFP/01_first_look.py
@@ -48,7 +48,7 @@ stim_sequence = {'sub-01':["no_stim-left rec", "no_stim-right rec", "Right stim-
                  'sub-05':["Left stim- no rec", "Right stim- no rec", "no_stim-left rec", "no_stim-right rec"]}  
 
 # BIDS settings
-subject = '02'
+subject = '03'
 session = '01'
 task = 'SpAtt'
 run = '01'
@@ -56,7 +56,7 @@ run = '01'
 # BIDS events
 events_suffix = 'events'  
 events_extension = '.tsv'
-base_fname = 'sub-02_ses-01_task-SpAtt_run-01_lfp'
+base_fname = 'sub-03_ses-01_task-SpAtt_run-01_lfp'
 
 platform = 'mac'  # are you using 'bluebear', 'mac', or 'windows'?
 pilot = False  # is it pilot data or real data?
@@ -78,7 +78,7 @@ events_fname = op.join(base_fpath, base_fname + '-eve.fif')
 annotated_raw_fname = op.join(base_fpath, base_fname + '_ann.fif')
 
 # Read raw file in BrainVision (.vhdr, .vmrk, .eeg) format
-raw = mne.io.read_raw_edf(lfp_fname, preload=False)
+raw = mne.io.read_raw_edf(lfp_fname, preload=True)
 raw.plot()  # first thing first
 
 # Remove the first few seconds while the LFP is warming up.
@@ -92,17 +92,19 @@ cropped_raw.info["line_freq"] = 50  # specify power line frequency as required b
 event values. We still have to proceed with this method as
 there's no other way to retrieve events from the
 raw object as there are no stim channels."""
-events, _ = mne.events_from_annotations(cropped_raw, event_id='auto')
+events_messed_up, _ = mne.events_from_annotations(cropped_raw, event_id='auto')
 
-events_unique = np.unique(events[:,2])  # just to check how many unique events are in the raw- should be equal to the next line
-events_unique_len = len(events)
+events_messed_up_unique = np.unique(events_messed_up[:,2])  # just to check how many unique events are in the raw- should be equal to the next line
+events_messed_up_unique_len = len(events_messed_up)
 annotations_unique = cropped_raw.annotations.count()  # both should have 10 unique events
 annotations_unique_len = cropped_raw.annotations
 
 # Create Annotation object with correct labels
 """use values in events variable. events_from_annotations assigns values from 1 to 
 however many events are in the raw file in some order I don't understand.
-(from mne: Map descriptions to unique integer values based on their sorted order.)"""
+(from mne: Map descriptions to unique integer values based on their sorted order.)
+Make sure to plot and check which value corresponds to which event and define 
+them in the mapping dictionary below."""
 
 mapping = {1:'cue_onset_right',
            4:'cue_onset_left',
@@ -116,29 +118,21 @@ mapping = {1:'cue_onset_right',
            3:'block_end',         
         }
 #annotations_from_events with mapping decreases events 217->170 and messes up the ids
-annotations_from_events = mne.annotations_from_events(events=events,
+annotations_from_events = mne.annotations_from_events(events=events_messed_up,
                                                     event_desc=mapping,
                                                     sfreq=cropped_raw.info["sfreq"],
                                                     orig_time=cropped_raw.info["meas_date"],
                                                     )
 cropped_raw.set_annotations(annotations_from_events)
 
-
-mne.write_events(events_fname, events, overwrite=True)  # write events in a separate file
-
-# Filter raw data (130Hz is stimulation frequency)
-cropped_raw.load_data().filter(0.3,100)  # data should be loaded
-
 # Plot to test
 cropped_raw.plot(title="raw") 
 n_fft = int(cropped_raw.info['sfreq']*2)  # to ensure window size = 2
-psd_fig = cropped_raw.copy().compute_psd(n_fft=n_fft,  # default method is welch here (multitaper for epoch)
+psd_fig = cropped_raw.copy().compute_psd(method='welch',
+                                         n_fft=n_fft,  # default method is welch here (multitaper for epoch)
                                          n_overlap=int(n_fft/2),
-                                         fmax=105).plot()  
+                                         fmax=1000).plot()  
                                                                                       
-# Save a non-bids raw just in case 
-cropped_raw.save(annotated_raw_fname, overwrite=True)  # note that the event_id is incorrect here, use the event_id dict if needed
-
 # Have to explicitly assign values to events for brainvision data
 """list of triggers https://github.com/tghafari/STN-stimulation-oscillation/blob/main/Instructions/triggers.md"""
 event_dict = {'cue_onset_right':1,
@@ -151,25 +145,29 @@ event_dict = {'cue_onset_right':1,
            'response_press_onset':8,
            'block_onset':20,
            'block_end':21,
-           #'experiment_end':30,  #sub02 does not have this
-           #'abort':31,  # participant 04_wmf has abort
-           #'new_stim_segment_maybe':10001,  # sub01 has an extra trigger
-           #'new_stim_segment':99999, 
-        }
-_, events_id = mne.events_from_annotations(raw, event_id=event_dict)
+            }
+events, events_id = mne.events_from_annotations(cropped_raw, event_id=event_dict)
+mne.write_events(events_fname, events, overwrite=True)  # write events in a separate file
+
+# Save a non-bids raw just in case 
+cropped_raw.save(annotated_raw_fname, overwrite=True)  # note that the event_id is incorrect here, use the event_id dict if needed
 
 # Convert to BIDS
-bids_path = BIDSPath(subject=subject, session=session, datatype ='eeg',
-                     task=task, run=run, root=bids_root)
+bids_path = BIDSPath(subject=subject, session=session,
+                     task=task, run=run, root=bids_root, datatype='eeg',  # BIDS does not accept LFP
+                     extension='.edf').mkdir(exist_ok=True)
+
 
 # Write to BIDS format
-raw.set_annotations(None)  # have to remove annotations to prevent duplicating when converting to BIDS
-write_raw_bids(raw, bids_path, events_data=events_fname, 
+cropped_raw.set_annotations(None)  # have to remove annotations to prevent duplicating when converting to BIDS
+write_raw_bids(cropped_raw, bids_path, events_data=events_fname, 
                event_id=events_id, overwrite=True, allow_preload=True,
-               format='BrainVision')
+               format='EDF')
 
 # Plot all events
-fig = mne.viz.plot_events(events, sfreq=raw.info["sfreq"], first_samp=raw.first_samp, event_id=events_id)
+fig = mne.viz.plot_events(events, sfreq=cropped_raw.info["sfreq"], 
+                          first_samp=cropped_raw.first_samp, 
+                          event_id=events_id)
 
 # Plot triggers from bids .tsv file
 events_bids_path = bids_path.copy().update(suffix=events_suffix,
@@ -204,25 +202,31 @@ plt.xticks(range(len(numbers_dict)), list(numbers_dict.keys()), rotation=45)
 ax.bar_label(bars)
 plt.show()
 
+
+# here needs a for loop to read the tsv file line by line and checks for 
+# all events between two trial_onsets
+# calculate reaction time
+# add event info + rt to a report called: sanity checks
+
 if sanity_test:
     # Check duration of cue presentation  
     events_dict['stim_to_dot_duration'] = events_dict['dot_onset'] - events_dict['stim_onset']
-    # events_dict['RT'] = events_dict['response_press_onset'] - events_dict['dot_onset']  # participants 01 and 04 only responded to right stim -> do not execute this line
+    events_dict['RT'] = events_dict['response_press_onset'] - events_dict['dot_onset_right']  # participants 01 and 04 only responded to right stim -> do not execute this line
     # cheat line to add extra elements for easier calculation of RTs:
     # events_dict['dot_onset'] = np.insert(events_dict['dot_onset'], index_onset_should_be_added_before, onset)
 
     # Plot  durations
-    events_dict["dur_cue_onset"] = events_dict['cue_onset'] - events_dict['trial_onset']
+    # events_dict["dur_cue_onset"] = events_dict['cue_onset'] - events_dict['trial_onset']
     fig, ax = plt.subplots()
-    plt.hist(events_dict["dur_cue_onset"])
-    plt.title("dur_cue_onset")
+    plt.hist(events_dict["RT"])
+    plt.title("RT")
     plt.xlabel('time in sec')
     plt.ylabel('number of events')
     plt.show()
 
 
 if summary_rprt:
-    report_root = op.join(project_root, 'derivatives/reports')  # RDS folder for reports
+    report_root = op.join(project_root, 'derivatives/reports/pilot')  # RDS folder for reports
    
     if not op.exists(op.join(report_root , 'sub-' + subject)):
         os.makedirs(op.join(report_root , 'sub-' + subject))
@@ -238,8 +242,8 @@ if summary_rprt:
                         event_id=events_id, 
                         tags=('eve'),
                         title='events from "events"', 
-                        sfreq=raw.info['sfreq'])
-    report.add_raw(raw=raw.filter(0.3, 100), title='raw with bad channels', 
+                        sfreq=cropped_raw.info['sfreq'])
+    report.add_raw(raw=cropped_raw.filter(0.3, 100), title='raw with bad channels', 
                    psd=True, 
                    butterfly=False, 
                    tags=('raw'))
