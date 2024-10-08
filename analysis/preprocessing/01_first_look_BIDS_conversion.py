8f33693395d43ae00e11ceeffeb926705338556e
tghafari <tara.ghafari@gmail.com>
2024-10-08 19:26:39 +0100

    minor edits on first look eeg

--- analysis/preprocessing/01_first_look_BIDS_conversion.py
+++ analysis/preprocessing/01_first_look_BIDS_conversion.py
@@ -37,7 +37,7 @@ from mne_bids import (BIDSPath, write_raw_bids, read_raw_bids)
 import matplotlib.pyplot as plt
 
 # # Fill these out - for older subjects- before 105
-# subj_code = '108'  # subject code assigned to by Benchi's group
+# subj_code = 'sub05'  # subject code assigned to by Benchi's group- only for subjects before 5 (inc)
 # base_fname = '5.15_5_AO'  # the name of the eeg file for 01_ly to sub05 should be manually copied here
 
 # Stimulation sequence
@@ -54,6 +54,7 @@ session = '01'
 task = 'SpAtt'
 run = '01'
 modality = 'eeg'
+extension = '.fif'
 
 platform = 'mac'  # are you using 'bluebear', 'mac', or 'windows'?
 pilot = False  # is it pilot data or real data?
@@ -77,7 +78,7 @@ base_fname = f'sub-{subject}_ses-{session}_task-{task}_run-{run}_{modality}'
 eeg_fname = op.join(base_fpath, base_fname + '.eeg')  
 vhdr_fname = op.join(base_fpath, base_fname + '.vhdr')
 events_fname = op.join(base_fpath, base_fname + '-eve.fif')
-annotated_raw_fname = op.join(base_fpath, base_fname + '_eeg.fif')
+annotated_raw_fname = op.join(base_fpath, base_fname + extension)
 
 # BIDS events
 events_suffix = 'events'  
@@ -86,7 +87,6 @@ bids_root = op.join(project_root, 'data', 'BIDS')
 
 # Read raw file in BrainVision (.vhdr, .vmrk, .eeg) format
 raw = mne.io.read_raw_brainvision(vhdr_fname, eog=('HEOGL', 'HEOGR', 'VEOGb'), preload=True)
-
 raw.plot()  # first thing first
 
 # Read events from raw object
@@ -103,7 +103,7 @@ mapping = {1:'cue_onset_right',
            8:'response_press_onset',
            20:'block_onset',
            21:'block_end',
-           #30:'experiment_end',
+           30:'experiment_end',
            #31: 'abort',  # participant 04_wmf has abort
            #10001:'new_stim_segment_maybe',  # sub01 has an extra trigger           
            99999:'new_stim_segment',
@@ -126,22 +126,12 @@ raw.set_eeg_reference(ref_channels=['Fz'], projection=False, verbose=False)
 montage = mne.channels.make_standard_montage("easycap-M1")
 raw.set_montage(montage, verbose=False)
 """
-
 mne.write_events(events_fname, events, overwrite=True)  # write events in a separate file
 
-# Filter raw data (130Hz is stimulation frequency)
-raw.filter(0.3,100)
-
-# Plot to test
-raw.plot(title="raw") 
-n_fft = int(raw.info['sfreq']*2)  # to ensure window size = 2
-psd_fig = raw.copy().compute_psd(n_fft=n_fft,  # default method is welch here (multitaper for epoch)
-                                 n_overlap=int(n_fft/2),
-                                 fmax=105).plot()  
 
 #sub-05 first 1300seconds are before the task starts.
-if subj_code == 'sub05':    
-    raw.crop(tmin=1380)
+# if subj_code == 'sub05':    
+#     raw.crop(tmin=1380)
                                                                                          
 # Save a non-bids raw just in case 
 raw.save(annotated_raw_fname, overwrite=True)  # note that the event_id is incorrect here, use the event_id dict if needed
@@ -157,7 +147,7 @@ event_dict = {'cue_onset_right':1,
            'response_press_onset':8,
            'block_onset':20,
            'block_end':21,
-           #'experiment_end':30,  #sub02 does not have this
+           'experiment_end':30,  #sub02 does not have this
            #'abort':31,  # participant 04_wmf has abort
            #'new_stim_segment_maybe':10001,  # sub01 has an extra trigger
            'new_stim_segment':99999, 
@@ -165,14 +155,29 @@ event_dict = {'cue_onset_right':1,
 _, events_id = mne.events_from_annotations(raw, event_id=event_dict)
 
 # Convert to BIDS
-bids_path = BIDSPath(subject=subject, session=session, datatype ='eeg',
-                     task=task, run=run, root=bids_root)
+bids_path = BIDSPath(subject=subject, 
+                     session=session, 
+                     datatype ='eeg',
+                     task=task, 
+                     run=run, 
+                     root=bids_root)
 
 # Write to BIDS format
 raw.set_annotations(None)  # have to remove annotations to prevent duplicating when converting to BIDS
-write_raw_bids(raw, bids_path, events_data=events_fname, 
-               event_id=events_id, overwrite=True, allow_preload=True,
+write_raw_bids(raw, 
+               bids_path, 
+               events=events_fname, 
+               event_id=events_id, 
+               overwrite=True, 
+               allow_preload=True,
                format='BrainVision')
+
+# Plot to test, filter raw data (130Hz is stimulation frequency)
+raw.copy().filter(0.3,100).plot(title="raw")  # does not contain triggers
+n_fft = int(raw.info['sfreq']*2)  # to ensure window size = 2sec
+psd_fig = raw.copy().filter(0.3,100).compute_psd(n_fft=n_fft,  # default method is welch here (multitaper for epoch)
+                                                n_overlap=int(n_fft/2),
+                                                fmax=105).plot()  
 
 # Plot all events
 fig = mne.viz.plot_events(events, sfreq=raw.info["sfreq"], first_samp=raw.first_samp, event_id=events_id)
@@ -204,7 +209,7 @@ for numbers in  ['cue_onset_right', 'cue_onset_left', 'dot_onset_right', 'dot_on
                         'response_press_onset']:
     numbers_dict[numbers] = events_dict[numbers].size
     
-fig, ax = plt.subplots()
+eve_fig, ax = plt.subplots()
 bars = ax.bar(range(len(numbers_dict)), list(numbers_dict.values()))
 plt.xticks(range(len(numbers_dict)), list(numbers_dict.keys()), rotation=45)
 ax.bar_label(bars)
@@ -245,6 +250,10 @@ if summary_rprt:
                         tags=('eve'),
                         title='events from "events"', 
                         sfreq=raw.info['sfreq'])
+        report.add_figure(eve_fig,
+                          title='Number of events',
+                          caption='number of events in total',
+                          tags=('eve'))
     report.add_raw(raw=raw.filter(0.3, 100), title='raw with bad channels', 
                    psd=True, 
                    butterfly=False, 
