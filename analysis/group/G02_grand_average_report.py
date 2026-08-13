efbd0a490ebbe5d0e785eaee591b3d177fde6e41
tghafari <tara.ghafari@gmail.com>
2026-08-14 00:56:02 +0100

    minor

--- analysis/group/G02_grand_average_report.py
+++ analysis/group/G02_grand_average_report.py
@@ -73,7 +73,7 @@ BIDS_ROOT = op.join(PROJECT_ROOT, "data", "BIDS")
 GROUP_REPORT_DIR = op.join(PROJECT_ROOT, "derivatives", "reports", "group", "grand_average")
 GROUP_DERIV_DIR = op.join(BIDS_ROOT, "derivatives", "group", "grand_average")
 
-REPORT_NAME = "group_grand_average"
+REPORT_NAME = "group_grand_average_"+ "_".join(subjects)
 REPORT_TITLE = "Grand average across subjects"
 
 OCCIPITAL_CHANNELS = ["PO3", "PO4", "POz"]
